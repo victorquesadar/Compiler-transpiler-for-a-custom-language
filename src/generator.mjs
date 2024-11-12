94cb1a684e6c0bb7dc1862fa437c0544e91c2638
NekoLi3 <estebanamg29@gmail.com>
2024-11-12 16:31:39 -0600

    itThenElse

--- src/generator.mjs
+++ src/generator.mjs
@@ -1,94 +1,99 @@
 let frameCount = 0;
-let variable=0; 
+let variable = 0;
 let variableCount = [];
-let ifCount = 0;  
+let ifCount = 0;
 
 class Identifier {
-  constructor(label, value = null) {
-    this.label = label;
-    this.value = value;
-   
-  }
+	constructor(label, value = null) {
+		this.label = label;
+		this.value = value;
+
+	}
 }
 
 function printPostOrder(node) {
-  if (!node) return;
+	if (!node) return;
 
-  // Si el nodo es un arreglo, recorre cada elemento recursivamente
-  if (Array.isArray(node)) {
-    node.forEach(printPostOrder);
-    return;
-  }
+	// Si el nodo es un arreglo, recorre cada elemento recursivamente
+	if (Array.isArray(node)) {
+		node.forEach(printPostOrder);
+		return;
+	}
 
-  // Recorre primero los hijos del nodo
-  if (node.children && node.children.length > 0) {
-    node.children.forEach(printPostOrder);
-  }
+	// Recorre primero los hijos del nodo
+	if (node.children && node.children.length > 0) {
+		node.children.forEach(printPostOrder);
+	}
 
-  // Imprime dependiendo del tipo de nodo
-  if (node.type === 'NUMBER') {
-    console.log(node.value);
-  } else if (node.type === 'PRINT') {
-    console.log('PRN');
-    if (node.value && node.value.children) {
-      node.value.children.forEach(printPostOrder);
-    }
-  }
-  
-  else if (node.type === 'IDENTIFIER') {
+	// Imprime dependiendo del tipo de nodo
+	if (node.type === 'NUMBER') {
+		console.log(node.value);
+	} else if (node.type === 'PRINT') {
+		console.log('PRN');
+		if (node.value && node.value.children) {
+			node.value.children.forEach(printPostOrder);
+		}
+	}
 
-    const existingIdentifier = variableCount.find(ident => ident.label === node.value.name);
-    console.log("BLD", frameCount, existingIdentifier.value);
+	else if (node.type === 'IDENTIFIER') {
 
-  } else if (node.type === 'LET') {
-    
+		const existingIdentifier = variableCount.find(ident => ident.label === node.value.name);
+		console.log("BLD", frameCount, existingIdentifier.value);
 
-    const existingIdentifier = variableCount.find(ident => ident.label === node.value.name);
-    if (!existingIdentifier) {
-      const identy = new Identifier(node.value.name,variable);
-      variableCount.push(identy); 
-      variable++;
-      console.log('BST',frameCount,identy.value);
-    } else {
-      console.error('Error: node.name is undefined');
-    }
-    
-  }else if(node.type === 'INT'){
-    console.log('LDV',node.value);
+	} else if (node.type === 'LET') {
 
-  } else if(node.type === 'STR'){
-    console.log('LDV',node.value);
 
-  }   else if(node.type === 'LIST'){
-    console.log('LDV', node.value);
-    }
-    else if (node.type === 'COMPARE') {
-    if (node.type === 'IF_THEN_ELSE') {
-      console.log('BTT', node.value);
-    } else {
-      console.log(node.value);
-    }
-    }
-    else if(node.type === 'blockStatement'){
-    ifCount=0;
-    console.log('BT ',node.value);
-    ifCount+=node.weight;
-    console.log(ifCount);
-  }
-  else if(node.type === 'IF_THEN_ELSE'){
- 
-    console.log('BF ',node.value);
-  
-  }
-  else if (node.type === 'ADD') {
-    console.log('ADD');
-  } else if (node.type === 'SUB') {
-    console.log('SUB');
-  } else if (node.type === 'MUL') {
-    console.log('MUL');
-  } else if (node.type === 'DIV') {
-    console.log('DIV');
-}
+		const existingIdentifier = variableCount.find(ident => ident.label === node.value.name);
+		if (!existingIdentifier) {
+			const identy = new Identifier(node.value.name, variable);
+			variableCount.push(identy);
+			variable++;
+			console.log('BST', frameCount, identy.value);
+		} else {
+			console.error('Error: node.name is undefined');
+		}
+
+	} else if (node.type === 'INT') {
+		console.log('LDV', node.value);
+
+	} else if (node.type === 'STR') {
+		console.log('LDV', node.value);
+
+	} else if (node.type === 'LIST') {
+		console.log('LDV', node.value);
+	}
+	else if (node.type === 'COMPARE') {
+
+		if (node.isIf) {
+			console.log(node.value);
+			console.log('BF', node.weight);
+			ifCount = 0;
+
+		} else {
+			//console.log(node.value);
+		}
+	}
+	else if (node.type === 'blockStatement') {
+
+		console.log('BT', node.weight);
+
+
+	}
+	else if (node.type === 'IF_THEN') {
+
+		ifCount = 0;
+
+	}
+	else if (node.type === 'ADD') {
+		console.log('ADD');
+	} else if (node.type === 'SUB') {
+		console.log('SUB');
+	} else if (node.type === 'MUL') {
+		console.log('MUL');
+	} else if (node.type === 'DIV') {
+		console.log('DIV');
+	}
+
 
 }
 
--- src/parser.mjs
+++ src/parser.mjs
@@ -2,121 +2,149 @@
 import JavaScriptVisitor from "../parser/JavaScriptVisitor.js";
 
 class ASTNode {
-  constructor(type, value = null, children = [], weight = 1) {
-    this.type = type;
-    this.value = value;
-    this.children = Array.isArray(children) ? children : [children];
-    this.weight = 1;
-  }
+	constructor(type, value = null, children = [], weight = 0) {
+		this.type = type;
+		this.value = value;
+		this.children = Array.isArray(children) ? children : [children];
+		this.weight = weight;
+	}
 }
 
 class MyVisitor extends JavaScriptVisitor {
-  visitPrintStatement(ctx) {
-    const message = this.visit(ctx.expression());
-    return new ASTNode("PRINT", null, [message]);
-  }
-  visitExpression(ctx) {
-    if (ctx.binaryOperation()) {
-      return this.visitBinaryOperation(ctx.binaryOperation());
-    } else if (ctx.NUMBER()) {
-      return new ASTNode("NUMBER", {
-        value: parseFloat(ctx.NUMBER().getText()),
-      });
-    } else if (ctx.additiveExpr()) {
-      return this.visitAdditiveExpr(ctx.visitAdditiveExpr());
-    }
-  }
+	visitPrintStatement(ctx) {
+		const message = this.visit(ctx.expression());
+		return new ASTNode("PRINT", null, [message]);
+	}
+	visitExpression(ctx) {
+		if (ctx.binaryOperation()) {
+			return this.visitBinaryOperation(ctx.binaryOperation());
+		} else if (ctx.NUMBER()) {
+			return new ASTNode("NUMBER", {
+				value: parseFloat(ctx.NUMBER().getText()),
+			});
+		}
+	}
 
-  visitDeclaration(ctx) {
-    const isConstant = ctx.getChild(0).getText() === "const"; // 'const' or 'let'
-    const variableAssignmentNode = this.visit(ctx.variableAssignment());
-    return new ASTNode("DECLARATION", { isConstant }, [variableAssignmentNode]);
-  }
-  visitBinaryOperation(ctx) {
-    const left = this.visit(ctx.multiplicativeExpression(0));
-    const right = this.visit(ctx.multiplicativeExpression(1));
-    const op = ctx.getChild(1).getText(); // "+" o "-"
-    return new ASTNode(op, null, [left, right]);
-  }
+	visitDeclaration(ctx) {
+		const isConstant = ctx.getChild(0).getText() === "const"; // 'const' or 'let'
+		const variableAssignmentNode = this.visit(ctx.variableAssignment());
+		return new ASTNode("DECLARATION", { isConstant }, [variableAssignmentNode]);
+	}
+	visitBinaryOperation(ctx) {
+		const left = this.visit(ctx.multiplicativeExpression(0));
+		const right = this.visit(ctx.multiplicativeExpression(1));
+		const op = ctx.getChild(1).getText(); // "+" o "-"
+		return new ASTNode(op, null, [left, right], 1);
+	}
 
-  // Visit for variable assignment (IDENTIFIER '=' expression)
-  visitVariableAssignment(ctx) {
-    const variableName = ctx.IDENTIFIER().getText(); // Get the variable name
-    const expressionValue = this.visit(ctx.expression()); // Visit the expression on the right side
-    return new ASTNode("LET", { name: variableName }, [expressionValue]);
-  }
-  visitNumberExpr(ctx) {
-    return new ASTNode("INT", parseInt(ctx.NUMBER().getText(), 10));
-  }
+	// Visit for variable assignment (IDENTIFIER '=' expression)
+	visitVariableAssignment(ctx) {
+		const variableName = ctx.IDENTIFIER().getText(); // Get the variable name
+		const expressionValue = this.visit(ctx.expression()); // Visit the expression on the right side
+		return new ASTNode("LET", { name: variableName }, [expressionValue], 1);
+	}
+	visitNumberExpr(ctx) {
 
-
-  visitParens(ctx) {
-    return this.visit(ctx.expr());
-  }
-  visitIdentifierExpr(ctx) {
-    const identifierName = ctx.getText();
-    return new ASTNode("IDENTIFIER", { name: identifierName });
-  }
-  visitAdditiveExpr(ctx) {
-    const left = this.visit(ctx.expression(0));
-    const right = this.visit(ctx.expression(1));
-    const operator = ctx.getChild(1).getText(); // Obtiene '+' o '-'
-    const operationType = operator === '+' ? 'ADD' : 'SUB';
-    return new ASTNode(operationType, null, [left, right]);
-  }
-  visitMultiplicativeExpr(ctx) {
-    const left = this.visit(ctx.expression(0));
-    const right = this.visit(ctx.expression(1));
-    const operator = ctx.getChild(1).getText(); // Obtiene '*' o '/'
-    const operationType = operator === '*' ? 'MUL' : 'DIV';
-    return new ASTNode(operationType, null, [left, right]);
-  }
-  
-  visitStringExpr(ctx) {
-    return new ASTNode("STR", ctx.STRING_LITERAL().getText());
-  }
+		return new ASTNode("INT", parseInt(ctx.NUMBER().getText(), 10), [], 1);
+	}
 
 
-  visitListExpression(ctx) {
-    return new ASTNode("LIST", ctx.getText());
-  } 
-  visitIfThenElseStatement(ctx) {
-    const condition = this.visit(ctx.expression(0));
-    const thenBranch = this.visit(ctx.ifBlockStatement(0));
-    const elseBranch = this.visit(ctx.elseBlockStatement(1)) ;
-    return new ASTNode("IF_THEN_ELSE", null, [condition, thenBranch, elseBranch]);
-  }
+	visitParens(ctx) {
+		return this.visit(ctx.expr());
+	}
+	visitIdentifierExpr(ctx) {
+		const identifierName = ctx.getText();
+		return new ASTNode("IDENTIFIER", { name: identifierName }, [], 1);
+	}
+	visitAdditiveExpr(ctx) {
+		const left = this.visit(ctx.expression(0));
+		const right = this.visit(ctx.expression(1));
+		const operator = ctx.getChild(1).getText(); // Obtiene '+' o '-'
+		const operationType = operator === '+' ? 'ADD' : 'SUB';
+		return new ASTNode(operationType, null, [left, right], 1);
+	}
+	visitMultiplicativeExpr(ctx) {
+		const left = this.visit(ctx.expression(0));
+		const right = this.visit(ctx.expression(1));
+		const operator = ctx.getChild(1).getText(); // Obtiene '*' o '/'
+		const operationType = operator === '*' ? 'MUL' : 'DIV';
+		return new ASTNode(operationType, null, [left, right], 1);
+	}
 
-  visitIfBlockStatement(ctx) {
-    const statements = ctx.statement().map(statement => this.visit(statement));
-  
-    return new ASTNode("blockStatement", null, statements);
-  }
+	visitStringExpr(ctx) {
+		return new ASTNode("STR", ctx.STRING_LITERAL().getText(), 1);
+	}
 
 
+	visitListExpression(ctx) {
+		return new ASTNode("LIST", ctx.getText());
+	}
+	visitIfThenElseStatement(ctx) {
+		const condition = this.visit(ctx.expression(0));
+		condition.isIf = true;
 
-  visitIfThenStatement(ctx) {
-    const condition = this.visit(ctx.expression(0));
-    const thenBranch = this.visit(ctx.blockStatement(0));
-    
-    return new ASTNode("IF_THEN", null, [condition, thenBranch]);
-  }
-  
-  visitCompareExpr(ctx) {
-    const left = this.visit(ctx.expression(0));
-    const right = this.visit(ctx.expression(1));
-    const operator = ctx.getChild(1).getText();  
-    const operationMap = {
-      '<': 'LT',
-      '>': 'GT',
-      '<=': 'LTE',
-      '>=': 'GTE',
-      '==': 'EQ',
-      '!=': 'NE'
-    };
-    const operationType = operationMap[operator] || 'UNKNOWN';
-    return new ASTNode("COMPARE", operationType, [left, right]);
-  }
+
+		const thenBranch = this.visit(ctx.ifBlockStatement(0));
+		
+		const elseBranch = this.visit(ctx.elseBlockStatement(1));
+
+		function countLeafNodes(node) {
+			// Desciende recursivamente en `node[0]` hasta encontrar un nodo sin `node[0]`
+			while (node[0] instanceof ASTNode || Array.isArray(node[0])) {
+				node = node[0];
+			}
+			let leafCount = 0;
+
+			// Condición para determinar si es una hoja: no tiene `children`
+			if (!node.children || node.children.length === 0) {
+				leafCount += node.weight;
+			} else if (node.weight > 0) {
+				leafCount += node.weight;
+			}
+
+			// Si tiene hijos en `children`, cuenta los nodos hoja en cada hijo recursivamente
+			return leafCount += node.children.reduce((count, child) => count + countLeafNodes(child), 0);
+		}
+
+	
+		condition.weight = countLeafNodes(thenBranch);
+		thenBranch.weight = countLeafNodes(elseBranch);
+		return new ASTNode("IF_THEN_ELSE", null, [condition, thenBranch, elseBranch]);
+	}
+
+	visitIfBlockStatement(ctx) {
+		const statements = ctx.statement().map(statement => this.visit(statement));
+
+		return new ASTNode("blockStatement", null, statements);
+	}
+
+	visitElseBlockStatement(ctx) {
+		const statements = ctx.statement().map(statement => this.visit(statement));
+		return new ASTNode("elseBlockStatement", null, statements);
+	}
+
+	visitIfThenStatement(ctx) {
+		const condition = this.visit(ctx.expression(0));
+		const thenBranch = this.visit(ctx.ifBlockStatement(0));
+
+		return new ASTNode("IF_THEN", null, [condition, thenBranch]);
+	}
+
+	visitCompareExpr(ctx) {
+		const left = this.visit(ctx.expression(0));
+		const right = this.visit(ctx.expression(1));
+		const operator = ctx.getChild(1).getText();
+		const operationMap = {
+			'<': 'LT',
+			'>': 'GT',
+			'<=': 'LTE',
+			'>=': 'GTE',
+			'==': 'EQ',
+			'!=': 'NE'
+		};
+		const operationType = operationMap[operator] || 'UNKNOWN';
+		return new ASTNode("COMPARE", operationType, [left, right], 1);
+	}
 }
 
 export { MyVisitor, ASTNode };
